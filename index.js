93f177bd5d316fd451339523a35f0114c55e8b09
Tiaan du Plessis <tiaanduplessis@hotmail.com>
2018-09-02 14:29:01 +0200

    updates

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 module.exports = {
-    parserOptions: {
-      ecmaVersion: 7,
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true,
-        modules: true,
-      },
-    },
-    env: {
-      browser: true,
-      node: true,
-      jest: true,
-    },
+    "parser": "babel-eslint",
     extends: ['airbnb', 'plugin:flowtype/recommended', 'plugin:jest/recommended', 'plugin:compat/recommended'],
-    rules: {
-      'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-      semi: [2, 'never'],
+    "plugins": [
+        "babel"
+    ],
+    env: {
+        browser: true,
+        node: true,
+        jest: true,
     },
-  }
-  
+    rules: {
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+        semi: [2, 'never'],
+    },
+}
